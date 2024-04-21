cc93740aa85febc3794c45779359f7eb0e897b8a
Deividas Kairiūkštis <kairiukstis.deividas@gmail.com>
2024-04-21 13:13:51 +0300

    new feature to add multiple dataPackets within single HTTP POST

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -16,16 +16,16 @@ class ApiController extends AbstractController
     public function createDataPacket(Request $request, ManagerRegistry $managerRegistry): Response
     {
         $params = $request->request;
-    
+
         if (!$params->has('authorizationToken') || !$params->has('batteryVoltage') || !$params->has('latitude') ||
             !$params->has('longitude') || !$params->has('eventId') || !$params->has('deviceId')) {
             return new Response('Wrong parameters');
         }
-        
+
         if ($this->getParameter('app.api_key') !== $params->get('authorizationToken')) {
             return new Response('Unauthorized');
         }
-        
+
         $em = $managerRegistry->getManager();
         $event = $em->getRepository(Event::class)->findOneBy([
             'id' => $params->get('eventId') //TODO: check if event is live
@@ -46,6 +46,58 @@ class ApiController extends AbstractController
 
         $em->persist($dataPacket);
         $em->flush();
+
+        return new Response('OK');
+    }
+
+    #[Route('/api/dataPackets', name: 'app_api_create_data_packets', methods: ['post'])]
+    public function createDataPackets(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $params = $request->request;
+
+        if (!$params->has('authorizationToken') || !$params->has('batteryVoltages') || !$params->has('latitudes') ||
+            !$params->has('longitudes') || !$params->has('eventId') || !$params->has('deviceId') ||
+            !$params->has('dataPacketsAmount') || !$params->has('dates')) {
+            return new Response('Wrong parameters');
+        }
+
+        if ($this->getParameter('app.api_key') !== $params->get('authorizationToken')) {
+            return new Response('Unauthorized');
+        }
+
+        $em = $managerRegistry->getManager();
+        $event = $em->getRepository(Event::class)->findOneBy([
+            'id' => $params->get('eventId') //TODO: check if event is live
+        ]);
+        if ($event == null) {
+            return new Response('Event not found');
+        }
+
+        $dataPacketsAmount = $params->get('dataPacketsAmount');
+        $latitudes = explode(';', $params->get('latitudes'));
+        $longitudes = explode(';', $params->get('longitudes'));
+        $batteryVoltages = explode(';', $params->get('batteryVoltages'));
+        $dates = explode(';', $params->get('dates'));
+
+        if (count($latitudes) != $dataPacketsAmount || count($longitudes) != $dataPacketsAmount ||
+            count($dates) != $dataPacketsAmount || count($batteryVoltages) != $dataPacketsAmount) {
+            return new Response('DataPackets amount mismatch');
+        }
+
+        for ($i = 0; $i < $dataPacketsAmount; $i++) {
+            $dataPacket = new DataPacket();
+            $dataPacket
+                ->setDate((new \DateTime($dates[$i]))->setTimezone(new \DateTimeZone('Europe/Vilnius')))
+                ->setBatteryVoltage($batteryVoltages[$i])
+                ->setLatitude($latitudes[$i])
+                ->setLongitude($longitudes[$i])
+                ->setDeviceId($params->get('deviceId'))
+                ->setEvent($event)
+            ;
+
+            $em->persist($dataPacket);
+            $em->flush();
+        }
 
         return new Response('OK');
     }
